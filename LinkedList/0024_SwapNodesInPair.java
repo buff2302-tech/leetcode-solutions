edbe56ec233d49968d5502c6a3d4d1431d461e91
sairamvasudevan <55596757+sairamvasudevan@users.noreply.github.com>
2025-09-27 22:34:13 +0530

    Create 0024_SwapNodesInPair.java

---
+++ LinkedList/0024_SwapNodesInPair.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        
+        ListNode temp = head;
+        int left;
+        int right;
+        while(temp != null && temp.next != null){
+            left = temp.val;
+            right = temp.next.val;
+            temp.val = right;
+            temp.next.val = left;
+            temp = temp.next.next;
+
+        }
+        return head;
+    }
+}
